0cf05a5d65045cbc1ed2077f2af882372037dba1
sravella <sravella@osius.com>
2024-11-12 14:57:59 +0530

    Add all project files to Git Repository

---
+++ src/main/java/com/springboot/website/SpringbootOnlineWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOnlineWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOnlineWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/website/SpringbootOnlineWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOnlineWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
